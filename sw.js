0d5e12f5cfc6c5b62e67dcc75898bc6e0cc7c205
heifets87-pixel <heifets87@gmail.com>
2026-05-25 22:02:55 +0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,10 @@
+const CACHE = "ideal-v2";
+self.addEventListener("install", e => {
+  self.skipWaiting();
+});
+self.addEventListener("activate", e => {
+  self.clients.claim();
+});
+self.addEventListener("fetch", e => {
+  e.respondWith(fetch(e.request).catch(() => caches.match(e.request)));
+});
